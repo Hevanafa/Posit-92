6c4a9450c2e78ef2e56e968f7fe711d1cbb52512
Hevanafa <dhevansafa4@gmail.com>
2026-08-27 19:33:23 +0700

    added file listing

--- latest/DEMOS/copy_cursor.pl
+++ latest/DEMOS/copy_cursor.pl
@@ -3,9 +3,24 @@ use warnings;
 use v5.38.2;
 
 use File::Copy;
+use File::Spec::Functions;
+
+my $src_file = catfile("SIMPLE", "CURSOR.BMP");
 
 opendir my $dh, ".";
 
-say for grep { -d && ($_ ne ".") && ($_ ne "..") } readdir $dh;
+for (grep { -d && ($_ ne ".") && ($_ ne "..") } readdir $dh) {
+  my $test_cursor = catfile($_, "CURSOR.BMP");
+
+  next if $test_cursor eq $src_file;
+
+  if (-f $test_cursor) {
+    say "Replacing ".$test_cursor
+
+    # TODO: Actually copy the cursor
+  }
+}
+
+closedir $dh
 
 # copy "SIMPLE/CURSOR.BMP"
